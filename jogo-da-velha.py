b8ded3cea62d44d40f763c0d838acee73798c0c8
fellipedepalma <fellipedepalma@gmail.com>
2025-07-30 00:21:07 -0300

    Criando - Tabuleiro e a função de mostrar o tabuleiro

--- jogo-da-velha.py
+++ jogo-da-velha.py
@@ -1,3 +1,14 @@
 # Jogo da Velha em Python por um iniciante
 
 # Criando um tabuleiro
+tabuleiro = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+def mostrar_tabuleiro(tabuleiro):
+    for i in tabuleiro:
+        for linha in i:
+            print(" | ",linha, end=" ")
+        print(" | ")
+
+mostrar_tabuleiro(tabuleiro)
+
+jogador_atual = "X"
