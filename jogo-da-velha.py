4b29f55a19684dfecb279c9c196c6951cf35d95f
fellipedepalma <fellipedepalma@gmail.com>
2025-07-30 17:05:14 -0300

    Teste

--- jogo-da-velha.py
+++ jogo-da-velha.py
@@ -1,4 +1,4 @@
-# Jogo da Velha em Python por um iniciante
+# JOGO DA VELHA EM PYTHON POR UM INICIANTE
 
 # CRIANDO UM TABULEIRO E JOGADOR ATUAL
 tabuleiro = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
@@ -9,24 +9,23 @@ jogadas_feitas = 0
 def mostrar_tabuleiro(tabuleiro):
     for i in tabuleiro:
         for linha in i:
-            print(" | ",linha, end=" ")
-        
+            print(" | ",linha, end=" ") 
         print(" | ")
         print(" | --- + --- + --- | ")
 
 # JOGADORES
 def mostrar_jogador_atual(jogador_atual):
     if jogador_atual == "X":
-        print("Jogador X")
+        print("JOGADOR X")
     else:
-        print("Jogador O")
+        print("JOGADOR O")
 
 # VALIDANDO A ENTRADA DA JOGADA
-def validando_entrada_jogada(jogada):
+def valida_entrada_jogada(jogada):
     if jogada in range(1, 10):
         return jogada
     else:
-        print("Jogada inválida")
+        print("JOGADA INVÁLIDA")
         return False
 
 # VERIFICANDO SE POSIÇÃO ESTÁ OCUPADA
@@ -50,12 +49,20 @@ while jogadas_feitas < 9:
     print("=======================================")
 
     mostrar_jogador_atual(jogador_atual)
+
     mostrar_tabuleiro(tabuleiro)
-    jogada_atual = validando_entrada_jogada(int(input("Digite um número de 1 a 9 correspondente ao tabuleiro: ")))
+    
+    jogada_atual = valida_entrada_jogada(int(input("Digite um número de 1 a 9 correspondente ao tabuleiro: ")))
+    print(jogada_atual)
 
+    esta_livre = posicao_livre(tabuleiro, jogada_atual)
 
-    posicao_livre(tabuleiro, jogada_atual)
-    atualiza_tabuleiro(tabuleiro, jogada_atual, jogador_atual)
+    print(esta_livre)
 
-    jogador_atual = 'O' if jogador_atual == 'X' else 'X'
-    jogadas_feitas += 1
+    if esta_livre:
+        atualiza_tabuleiro(tabuleiro, jogada_atual, jogador_atual)
+
+        jogador_atual = 'O' if jogador_atual == 'X' else 'X'
+        jogadas_feitas += 1
+    else:
+        print("FAÇA UMA JOGA VÁLIDA!")
